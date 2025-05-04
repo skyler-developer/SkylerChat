634833ec63a6db9222726aa173b3037268d5fd25
skyler <skylerhaut@foxmail.com>
2025-05-04 18:21:15 +0800

    feat: 会话历史

--- src/app/components/functionArea/index.tsx
+++ src/app/components/functionArea/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Image } from "antd";
 import { useUserInfoStore } from "@/store/useUserInfoStore";
+import { useSessionInfoStore } from "@/store/useSessionInfoStore";
 import styles from "./index.module.css";
+
 export default function FunctionArea() {
     const { username, isLogin } = useUserInfoStore();
+    const { sessionInfo } = useSessionInfoStore();
+    const [showSessionList, setShowSessionList] = useState(true);
     console.log("FunctionArea", username);
     return (
         <div style={{ display: "flex", flexDirection: "column", flex: 1 }}>
@@ -16,9 +20,30 @@ export default function FunctionArea() {
                 </div>
                 <div className={styles.horizontalLine} />
                 <div className={styles.buttonContainer}>
-                    <Button className={styles.button}>最近对话</Button>
+                    <Button className={styles.button} onClick={() => setShowSessionList((v) => !v)}>
+                        最近对话
+                    </Button>
+                </div>
+                <div>
+                    <div
+                        // 最终效果是 class="a b"
+                        className={
+                            styles.sessionContainer +
+                            " " +
+                            (showSessionList
+                                ? styles.sessionContainerOpen
+                                : styles.sessionContainerClose)
+                        }>
+                        {Array.isArray(sessionInfo) &&
+                            sessionInfo?.map((item, index) => (
+                                <div key={index} className={styles.sessionItem}>
+                                    {item.userName} {item.timeStamp}
+                                </div>
+                            ))}
+                    </div>
                 </div>
             </div>
+
             <div className={styles.userInfo}>
                 <div className={styles.userImg}>
                     <Image
